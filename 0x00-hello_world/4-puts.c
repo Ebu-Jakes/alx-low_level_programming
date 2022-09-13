9ac7950d784e0e2b3759e151513aa0b53c798b1f
Ebu-Jakes <ebujakes@gmail.com>
2022-09-12 19:50:17 -0700

    Solution of task 0 to 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: Beginning of the code
+ * Description: the program prints "Programming is like building a multilingual puzzle" followed by  a new line
+ * put is used rather than printf
+ * returns 0 : indicating success.
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: start of program
+ * description: the program that prints exactly " with proper grammar, but the outcome is a piece of art"
+ * returns 0 indicating the end of program
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
